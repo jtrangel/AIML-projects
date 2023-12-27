4e05f998604bad2d37bf3c26ee8ba039971ca145
Jerico Rangel <jerico.miggy.rangel@gmail.com>
2023-12-28 01:59:36 +0800

    initial speech-to-text project commit

---
+++ speech-to-text-transcription/main.py
@@ -0,0 +1,41 @@
+import assemblyai as aai
+
+aai.settings.api_key = "YOUR_API_KEY"
+
+
+def on_open(session_opened: aai.RealtimeSessionOpened):
+    print("Session ID:", session_opened.session_id)
+
+
+def on_data(transcript: aai.RealtimeTranscript):
+    if not transcript.text:
+        return
+
+    if isinstance(transcript, aai.RealtimeFinalTranscript):
+        print(transcript.text, end="\r\n")
+    else:
+        print(transcript.text, end="\r")
+
+
+def on_error(error: aai.RealtimeError):
+    print("An error occured:", error)
+
+
+def on_close():
+    print("Closing Session")
+
+
+transcriber = aai.RealtimeTranscriber(
+    sample_rate=16_000,
+    on_data=on_data,
+    on_error=on_error,
+    on_open=on_open,
+    on_close=on_close,
+)
+
+transcriber.connect()
+
+microphone_stream = aai.extras.MicrophoneStream(sample_rate=16_000)
+transcriber.stream(microphone_stream)
+
+transcriber.close()
---
+++ speech-to-text-transcription/readme.md
@@ -0,0 +1,2 @@
+Reference documentation: https://www.assemblyai.com/docs/getting-started/transcribe-streaming-audio-from-a-microphone-in-python
+
